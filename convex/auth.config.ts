1f44603f6a2f3e8c7b7d9a6ab1c2b8bceb0ba997
EucTech <euccreative@gmail.com>
2024-07-09 14:12:00 +0100

    updated

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://closing-grub-16.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
-import { useState } from "react";
 
 const TasksPage = () => {
     const tasks = useQuery(api.tasks.getTasks);
